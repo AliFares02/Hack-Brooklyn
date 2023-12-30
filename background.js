e35b6508a6de1edbd82cd8aaae6e60e251253b76
Ali-Fares <faresa0202@gmail.com>
2023-12-29 22:06:57 -0500

    Utilized chrome local storage instead of runtime onMessage as that was too buggy, also added text summary feature and more

--- background.js
+++ background.js
@@ -1,22 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request && request.url) {
     console.log('URL from content script:', request.url);
+    
+    chrome.storage.local.set({ webUrl: request.url });
   }
+  
 
-  if (request && request.bookmarkCurrentTab) {
-    bookmarkCurrentTab();
-  }
+ 
 });
-
-function bookmarkCurrentTab() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    if (activeTab) {
-      chrome.bookmarks.create({ url: activeTab.url, title: activeTab.title });
-      console.log('Tab bookmarked:', activeTab.url);
-    } else {
-      console.error('Error retrieving active tab information.');
-    }
-  });
-}
-
--- popup.js
+++ popup.js
@@ -1,16 +1,97 @@
-function handleMessages(request) {
-  if (request && request.urlFromContent) {
-    const urlFromContent = request.urlFromContent;
-    console.log('URL from content script:', urlFromContent);
-  }
-}
 
-function bookmarkCurrentTab() {
-  chrome.runtime.sendMessage({ bookmarkCurrentTab: true });
-}
+let isDivAdded = false;
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('bookmarkButton').addEventListener('click', bookmarkCurrentTab);
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.local.get('webUrl', async function (result) {
+    const urlFromContent = result.webUrl;
+    console.log('Stored URL in popup.js:', urlFromContent);
+  
+    let summarizedResult;
+
+    
+
+  const extractButtonElement = document.querySelector('.extractButton');
+ 
+  extractButtonElement.addEventListener('click', async () => {
+    const loadingElement = document.createElement('div');
+    loadingElement.textContent = 'Loading...';
+    loadingElement.style.fontWeight = 'bold';
+    loadingElement.style.color = 'gray';
+    document.body.appendChild(loadingElement);
+    
+    if (urlFromContent) {
+      console.log('Current URL:', urlFromContent);
+
+      const extractedUrl = 'https://article-data-extraction-and-summarization.p.rapidapi.com/article?url=' + urlFromContent;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '61e3269048msh2c25d7d20c98816p1e4b48jsna8efefd63e9f',
+          'X-RapidAPI-Host': 'article-data-extraction-and-summarization.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const extractedResponse = await fetch(extractedUrl, options);
+        const extractedResult = await extractedResponse.json();
+        const parsedText = extractedResult.article.text;
+        const cleanedText = parsedText.replace(/[\r\n]+/g, ' ').replace(/\s+/g, ' ').trim();
+        console.log('cleaned text:', cleanedText);
+
+        const summarizedUrl = 'https://chatgpt-42.p.rapidapi.com/conversationgpt4';
+        const summarizedOptions = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': '701dedb334msh71418a42e3b3030p13182ejsn54c99aad1f8b',
+          'X-RapidAPI-Host': 'chatgpt-42.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+          messages: [
+            {
+              role: 'user',
+              content: cleanedText
+            }
+          ],
+          system_prompt: 'Summarize this text',
+          temperature: 0.5,
+          top_k: 50,
+          top_p: 0.9,
+          max_tokens: 200,
+          web_access: false
+        })
+        };
+
+      try {
+        loadingElement.textContent = 'Fetching summary...';
+
+        const response = await fetch(summarizedUrl, summarizedOptions);
+        summarizedResult = await response.json();
+        console.log(summarizedResult.result);
+        loadingElement.textContent = '';
+      } catch (error) {
+        console.error(error);
+      }
+      } catch (error) {
+        console.error('Error in text extraction:', error);
+      }
+    if (!isDivAdded) {
+      const newDiv = document.createElement('div');
+      newDiv.style.backgroundColor = 'white';
+      newDiv.style.padding = '4px';
+      newDiv.style.border = '1px solid black';
+      newDiv.style.borderRadius = '4px';
+      newDiv.style.maxHeight = '200px';
+      newDiv.style.overflowY = 'auto';
+      newDiv.textContent = summarizedResult.result;
+      document.body.appendChild(newDiv);
+      isDivAdded = true;
+    }
+     } else {
+      console.error('URL from content script not available.');
+     }
+    
 });
+});
+})
 
-chrome.runtime.onMessage.addListener(handleMessages);
